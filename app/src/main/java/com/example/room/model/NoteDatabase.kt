2ebb1fc61c567887c13c1bb5e315ca34c19ba954
iZeroKim <Kimgichuru6@gmail.com>
2020-09-17 15:32:39 +0300

    Created Note Database

---
+++ app/src/main/java/com/example/room/model/NoteDatabase.kt
@@ -0,0 +1,36 @@
+package com.example.room.model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+// Annotates class to be a Room Database with a table (entity) of the Word class
+@Database(entities = arrayOf(Note::class), version = 1, exportSchema = false)
+public abstract class NoteDatabase : RoomDatabase() {
+
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: NoteDatabase? = null
+
+        fun getDatabase(context: Context): NoteDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NoteDatabase::class.java,
+                    "note_database"
+                ).fallbackToDestructiveMigration().build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
